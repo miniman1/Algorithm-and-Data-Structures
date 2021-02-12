4e18e0c79887035e44e9fcabf2e3ca0b1bc01b65
Duy Minh Nguyen <greatwhitepup@hotmail.com>
2021-02-13 07:41:29 +1100

    added method to create data structure

--- UnionFind/UnionFind.h
+++ UnionFind/UnionFind.h
@@ -1,6 +1,8 @@
+// return pointer to new UnionFind object, with 'size' amount of sets, and sets named from 0 to size-1 
+UnionFind *newUnionFind(int size); 
 
 // given element x, determine what set x belongs to, whilst applying path compression if necessary
 int find(UnionFind* U, int x);
     
-// given two elements x and y, merge the sets together
+// given two sets, merge the sets together
 void union(UnionFind* U, int x, int y); 
