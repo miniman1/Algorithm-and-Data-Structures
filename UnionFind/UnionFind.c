c50349f0626083eb3842a94beb0e1d693eda577d
Duy Minh Nguyen <greatwhitepup@hotmail.com>
2021-02-13 08:35:26 +1100

    completed newUnionFind function

--- UnionFind/UnionFind.c
+++ UnionFind/UnionFind.c
@@ -7,3 +7,20 @@ typedef Struct UnionFind {
     int *id; 
 } UnionFind; 
 
+
+UnionFind *newUnionFind(int size) {
+    UnionFind *U = malloc(sizeof(UnionFind));
+    U->size = size; 
+    U->id = malloc(sizeof(int) * size); 
+    // initially, the parent of each value is itself
+    for (int i = 0; i < size; i++) {
+        U->id[i] = i; 
+    }
+    return size; 
+}
+
+int find(UnionFind* U, int x);
+    
+int union(UnionFind* U, int x, int y); 
+
+void freeUnionFind(UnionFind* U); 
