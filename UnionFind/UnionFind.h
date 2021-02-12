5ab2d89f11f559ff45e4fc82851421488d867db8
Duy Minh Nguyen <greatwhitepup@hotmail.com>
2021-02-13 08:11:43 +1100

    added free function

--- UnionFind/UnionFind.h
+++ UnionFind/UnionFind.h
@@ -3,9 +3,12 @@
 UnionFind *newUnionFind(int size); 
 
 // given element x, determine what set x belongs to, whilst applying path compression if necessary
-// if x is invalid, return -1 
+// if x or U is invalid, return -1 
 int find(UnionFind* U, int x);
     
-// given two sets, merge the sets together
-// if x or y is invalid, do nothing
-void union(UnionFind* U, int x, int y); 
+// given two sets, merge the sets together, return true if successful
+// if x or y is invalid, return false
+int union(UnionFind* U, int x, int y); 
+
+void freeUnionFind(UnionFind* U); 
+
