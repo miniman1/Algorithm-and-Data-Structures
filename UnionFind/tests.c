b90c243a974e81a291cb5198947c9f1870655fba
Duy Minh Nguyen <greatwhitepup@hotmail.com>
2021-02-13 07:49:36 +1100

    comments / skeleton for test cases

---
+++ UnionFind/tests.c
@@ -0,0 +1,28 @@
+#include "UnionFind.h"
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char* argv[]) {
+    // invalid size
+        // invalid find
+        // valid find
+
+        // invalid union
+        // valid union 
+
+    // size == 1
+        // invalid find
+        // valid find
+
+        // invalid union
+        // valid union 
+
+    // size > 1 (e.g. 4)
+        // invalid find
+        // valid find
+
+        // invalid union
+        // valid union 
+}
+
