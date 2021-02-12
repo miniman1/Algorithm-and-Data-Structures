c9ab243f190c14c61e2102d7930d99884b19fcef
Duy Minh Nguyen <greatwhitepup@hotmail.com>
2021-02-13 07:24:38 +1100

    skeleton code for basic functionality

---
+++ UnionFind/UnionFind.h
@@ -0,0 +1,14 @@
+
+typedef Struct UnionFind {
+
+} UnionFind; 
+
+// given element x, determine what set x belongs to, whilst applying path compression if necessary
+int find(UnionFind* U, int x) {
+    
+}
+
+// given two elements x and y, merge the sets together
+void union(UnionFind* U, int x, int y) {
+
+}
