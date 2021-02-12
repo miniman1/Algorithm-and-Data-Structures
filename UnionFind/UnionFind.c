2551a5aedd9e766840302b576828b3821adf0100
Duy Minh Nguyen <greatwhitepup@hotmail.com>
2021-02-13 08:29:28 +1100

    defined the adt struct

--- UnionFind/UnionFind.c
+++ UnionFind/UnionFind.c
@@ -1,3 +1,9 @@
-typedef Struct UnionFind {
+#include <stdio.h>
+#include <stdlib.h>
 
+typedef Struct UnionFind {
+    int size; 
+    // tracks the parent of each value
+    int *id; 
 } UnionFind; 
+
